afa2c387c6cf0f74f80fd497561348fbcd76a27f
nickpro79 <nikhilpraveen7777@gmail.com>
2026-01-25 18:05:44 +0530

    completed authentication

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
+
 const app = express();
 
 app.use(helmet());
@@ -24,4 +26,15 @@ app.get('/', (req, res) => {
 logger.info('Hello from acqiusitons');
   res.status(200).send('Hello, World!fgsdhth');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
+
+app.get('/api',(req, res)=> {
+  res.status(200).json({message:'Acquisitions API is running'});
+})
+
+app.use('/api/auth', authRoutes);
+
 export default app;
