ad9625fcd5cc75929732d7e28a79ba4ae177994a
nickpro79 <nikhilpraveen7777@gmail.com>
2026-02-03 20:59:14 +0530

    implemented USER CRUD, need to fix bugs

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import userRoutes from '#routes/user.routes.js';
 import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
@@ -38,6 +39,6 @@ app.get('/api',(req, res)=> {
   res.status(200).json({message:'Acquisitions API is running'});
 })
 
-app.use('/api/auth', authRoutes);
-
+  app.use('/api/auth', authRoutes);
+  app.use('/api/users', userRoutes);
 export default app;
